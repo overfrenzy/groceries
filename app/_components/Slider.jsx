faa0d67e04c9f73498e2fac224dde497c76d2db0
overfrenzy <54790344+overfrenzy@users.noreply.github.com>
2024-09-15 13:29:18 +0900

    slider back

--- app/_components/Slider.jsx
+++ app/_components/Slider.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 function Slider({ sliderList }) {
   return (
-    <div className="carousel">
-      {sliderList.map((slider) => (
-        <div key={slider.id}>
-          <Image
-            src={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}${slider.image}`}
-            width={1000}
-            height={400}
-            alt="slider"
-            className="w-full h-[400px] object-cover rounded-2xl"
-          />
-        </div>
-      ))}
-    </div>
+    <Carousel>
+      <CarouselContent>
+        {sliderList.map((slider) => (
+          <CarouselItem key={slider.id}>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}${slider.image}`}
+              width={1000}
+              height={400}
+              alt="slider"
+              className="w-full h-[200px] md:h-[400px] object-cover rounded-2xl"
+            />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 }
 
